141c7dc37b55fc52d4422058feff979bb6f520f5
Full Name <oldster189@gmail.com>
2025-10-28 15:21:30 +0700

    134

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4444;
 const LOG_FILE = path.join(__dirname, 'logs.txt');
 
 // Middleware to parse JSON body
